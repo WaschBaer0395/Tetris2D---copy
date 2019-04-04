d3e1cddee7b6f46e47add8fe165a34bd02f83742
cmdr-WaschBaer <rene-hippe@live.de>
2019-04-04 15:22:41 +0200

    Update Controller.java

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -34,58 +34,36 @@ public class Controller implements Initializable {
     @FXML
     protected GridPane gamePane;
 
-    final int[][] TSpielfigur = {{0,1,0}, {1,1,1}, {0,0,0},{0,0,0}};
-    final int[][] ISpielfigur = {{0,1,0}, {0,1,0}, {0,1,0},{0,1,0}};
-    final int[][] OSpielfigur = {{1,1,0}, {1,1,0}, {0,0,0},{0,0,0}};
-    final int[][] ZSpielfigur = {{1,1,0}, {0,1,1}, {0,0,0},{0,0,0}};
-    final int[][] SSpielfigur = {{0,1,1}, {1,1,0}, {0,0,0},{0,0,0}};
-    final int[][] LSpielfigur = {{1,0,0}, {1,0,0}, {1,1,0},{0,0,0}};
-    final int[][] JSpielfigur = {{0,0,1}, {0,0,1}, {0,1,1},{0,0,0}};
-
-    boolean newFig = false;
+    final char[][] TSpielfigur = {{' ','T',' '}, {'T','T','T'}, {' ',' ',' '},{' ',' ',' '}};
+    final char[][] ISpielfigur = {{' ','I',' '}, {' ','I',' '}, {' ','I',' '},{' ','I',' '}};
+    final char[][] OSpielfigur = {{'O','O',' '}, {'O','O',' '}, {' ',' ',' '},{' ',' ',' '}};
+    final char[][] ZSpielfigur = {{'Z','Z',' '}, {' ','Z','Z'}, {' ',' ',' '},{' ',' ',' '}};
+    final char[][] SSpielfigur = {{' ','S','S'}, {'S','S',' '}, {' ',' ',' '},{' ',' ',' '}};
+    final char[][] LSpielfigur = {{'L',' ',' '}, {'L',' ',' '}, {'L','L',' '},{' ',' ',' '}};
+    final char[][] JSpielfigur = {{' ',' ','J'}, {' ',' ','J'}, {' ','J','J'},{' ',' ',' '}};
+    Color color;
 
 
     protected int posY = 0;
     protected int posX = 3;
 
-    protected int[][]field=new int[21][10];
-    protected Node[][] playfield = new Node[21][10];
+    protected char[][]field=new char[21][10];
 
     final static int maxR = 20;
     final static int maxC = 9;
-    protected int[][]figur = new int[4][3];
-    protected Rectangle[][] figArray = new Rectangle[4][3];
 
-    protected  int rotation = 0;
+    protected char[][]figur = new char[3][2];
+    boolean newFig = false;
+
+
 
 
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
-        figArray = randomizedFig();
-
-        for (int y = 0; y < figur.length; y++) {
-            for (int x = 0; x < figur[y].length; x++) {
-                if (figur[y][x] == 1) {
-                    figArray[y][x] = new Rectangle(33, 33, Color.BLACK);
-                }
-            }
-        }
-
-        for (int y = 0; y < 4; y++) {
-            for (int x = 0; x < 3; x++) {
-                if(figArray[y][x]!=null) {
-                    Rectangle rectangle = new Rectangle(33,33,Color.BLACK);
-                    GridPane.setRowIndex(rectangle, posY + y);
-                    GridPane.setColumnIndex(rectangle, posX + x);
-                    gamePane.getChildren().addAll(rectangle);
-                    playfield[posY+y][posX+x] = rectangle;
-                }
-            }
-        }
-
+        createNewFigurine();
+        drawField(field);
         registerKeyboardHandler(gamePane);
     }
 
@@ -94,7 +72,6 @@ public class Controller implements Initializable {
             @Override
             public void handle(KeyEvent keyEvent) {
                 boolean consumed = false;
-                newFig = false;
 
                 switch (keyEvent.getCode()) {
                     case DOWN:
@@ -163,36 +140,28 @@ public class Controller implements Initializable {
 
         switch(event){
             case "DOWN":
-                if(!changePosition(posY+1,posX)){
                     posY++;
-                }
                 if (newFig) {
                     createNewFigurine();
                 }
                 break;
 
             case"UP":
-                if(!changePosition(posY-1,posX)){
                     posY--;
-                }
                 if (newFig) {
                     createNewFigurine();
                 }
                 break;
 
             case"LEFT":
-                if(!changePosition(posY,posX-1)){
                     posX--;
-                }
                 if (newFig) {
                     createNewFigurine();
                 }
                 break;
 
             case"RIGHT":
-                if(!changePosition(posY,posX+1)){
                     posX++;
-                }
                 if (newFig) {
                     createNewFigurine();
                 }
@@ -202,9 +171,6 @@ public class Controller implements Initializable {
                 break;
 
             case "Q":
-                if(rotation==360) rotation=0;
-                else rotation=rotation+90;
-                changePosition(posY,posX);
                 break;
 
             case "E":
@@ -214,43 +180,99 @@ public class Controller implements Initializable {
         return endOfField;
     }
 
-    private void placeNode(int newPosY,int newPosX){
-        Rectangle rectangle = new Rectangle(33,33,Color.BLACK);
-        GridPane.setRowIndex(rectangle, newPosY );
-        GridPane.setColumnIndex(rectangle, newPosX);
-        gamePane.getChildren().addAll(rectangle);
-        playfield[newPosY][newPosX] = rectangle;
+    public void drawField(char[][] _field){
+        for(int y=0;y<=maxR;y++){
+            for(int x=0;x<=maxC;x++){
+                getTypeOfBlock(_field[y][x],y,x);
+            }
+        }
     }
 
-    private void clearPane(){
+    private void getTypeOfBlock(char block,int _y,int _x){
+        Rectangle rectangle;
+
+        switch(block){
+            case 'T':
+                rectangle = new Rectangle(33,33,Color.PURPLE);
+                rectangle.setStroke(Color.BLACK);
+                rectangle.setArcWidth(10.0);
+                rectangle.setArcHeight(10.0);
+
+                break;
+            case 'I':
+                rectangle = new Rectangle(33,33,Color.SKYBLUE);
+                rectangle.setStroke(Color.BLACK);
+                rectangle.setArcWidth(10.0);
+                rectangle.setArcHeight(10.0);
+
+                break;
+            case 'O':
+                rectangle = new Rectangle(33,33,Color.YELLOW);
+                rectangle.setStroke(Color.BLACK);
+                rectangle.setArcWidth(10.0);
+                rectangle.setArcHeight(10.0);
+
+                break;
+            case 'S':
+                rectangle = new Rectangle(33,33,Color.GREEN);
+                rectangle.setStroke(Color.BLACK);
+                rectangle.setArcWidth(10.0);
+                rectangle.setArcHeight(10.0);
+
+                break;
+            case 'Z':
+                rectangle = new Rectangle(33,33,Color.RED);
+                rectangle.setStroke(Color.BLACK);
+                rectangle.setArcWidth(10.0);
+                rectangle.setArcHeight(10.0);
+
+                break;
+            case 'J':
+                rectangle = new Rectangle(33,33,Color.BLUE);
+                rectangle.setStroke(Color.BLACK);
+                rectangle.setArcWidth(10.0);
+                rectangle.setArcHeight(10.0);
+
+                break;
+            case 'L':
+                rectangle = new Rectangle(33,33,Color.ORANGE);
+                rectangle.setStroke(Color.BLACK);
+                rectangle.setArcWidth(10.0);
+                rectangle.setArcHeight(10.0);
+
+                break;
+            default:
+                rectangle = new Rectangle(33,33,Color.TRANSPARENT);
+                break;
+        }
+            placeNode(rectangle,_y,_x);
+    }
+
+    public void placeNode(Rectangle _rectangle,int _y,int _x){
+        GridPane.setRowIndex(_rectangle, _y );
+        GridPane.setColumnIndex(_rectangle, _x);
+        gamePane.getChildren().addAll(_rectangle);
+    }
+
+    private void clearField(){
         Node node = gamePane.getChildren().get(0);
         gamePane.getChildren().clear();
         gamePane.getChildren().add(0,node);
     }
 
-    private void rewritePane(){
-        for(int y=0;y<playfield.length;y++){
-            for(int x=0;x<playfield[0].length;x++){
-                if(playfield[y][x]!=null) placeNode(y,x);
-            }
-        }
-
-    }
-
-    private boolean changePosition(int newPosY,int newPosX){
-        boolean collision = false;
+    /*private void changePosition(int oldPosY,int oldPosX,int newPosY,int newPosX){
         clearPane();
+        rewritePane();
         for (int y = 0; y < 4; y++) {
             for (int x = 0; x < 3; x++) {
                 if (figArray[y][x] != null) {
-                    collision = rotate(y,x);
+                    playfield[oldPosY + y][oldPosX + x] = null;
+                    placeNode(newPosY + y, newPosX + x);
                 }
             }
         }
-        rewritePane();
-        return collision;
     }
-
+*/
 
 
     @FXML
@@ -273,40 +295,40 @@ public class Controller implements Initializable {
         }
     }
 
-    protected  boolean checkForCollision(int newY, int newX){
-        boolean wallCollision = false;
-        boolean oCollision = false;
-        boolean eofCollision = false;
+    /*protected  boolean checkForCollision(int newY, int newX){
         boolean collision = false;
-
         Node[][]buffer = playfield;
 
-
-        buffer[posY][posX] = null;
-
-
-        if (wallCollision(newX)) {
-            wallCollision = true;
-        }else if(objectCollisionHor(buffer,posY,newX)) {
-            oCollision = true;
-        }else if(endOfField(newY)){
-            newFig = true;
-            eofCollision = true;
-        }else if (objectCollisionDown(buffer,newY,posX)) {
-            newFig = true;
-            oCollision = true;
+        for (int y = 0; y < 4; y++) {
+            for (int x = 0; x < 3; x++) {
+                if(figArray[y][x]!=null) {
+                    buffer[posY+y][posX+x] = null;
+                }
+            }
         }
-        else collision = false;
-
-        if(eofCollision || oCollision || wallCollision){
-            collision = true;
-        }
-        else {
-            playfield[posY][posX] = null;
-            placeNode(newY, newX);
+        for (int y = 0; y < 4; y++) {
+            for (int x = 0; x < 3; x++) {
+                if(figArray[y][x]!=null) {
+                    if (wallCollision(newX + x)) {
+                        collision = true;
+                        break;
+                    } else if(objectCollisionHor(buffer,newX +x,posY+y)){
+                        collision = true;
+                        break;
+                    } else if(endOfField(newY+y)){
+                        newFig = true;
+                        collision = true;
+                        break;
+                    } else if (objectCollisionVer(buffer,posX+x, newY + y)) {
+                        newFig = true;
+                        collision = true;
+                        break;
+                    }
+                }
+            }
         }
         return collision;
-    }
+    }*/
 
     protected boolean wallCollision(int _newCol){
         if(_newCol>maxC || _newCol<0){
@@ -315,52 +337,46 @@ public class Controller implements Initializable {
         return false;
     }
 
-    protected boolean objectCollisionDown(Node[][] buffer,int _newY,int _x){
-            if(buffer[_newY][_x] != null){
-                return true;
-            }
-            else return false;
-    }
-
-    protected boolean objectCollisionHor(Node[][] buffer,int _Y,int _newX){
-        if(buffer[_Y][_newX] != null){
+    protected boolean objectCollisionHor(Node[][] buffer,int _newCol,int _newRow){
+        if(buffer[_newRow][_newCol] != null){
             return true;
         }
         else return false;
     }
 
-    protected  boolean endOfField(int _newRow){
-        if(_newRow<=maxR){
-            return false;
+    protected boolean objectCollisionVer(Node[][] buffer,int _newCol ,int _newRow){
+        if(buffer[_newRow][_newCol] != null){
+            return true;
         }
-        else return true;
+        else return false;
+    }
+
+
+    protected  boolean endOfField(int _newRow){
+        if(_newRow>maxR){
+            return true;
+        }
+        else return false;
     }
 
     protected void createNewFigurine(){
-        for (int y = 0; y < figArray.length; y++) {
-            for (int x = 0; x < figArray[y].length; x++) {
-                if(figArray[y][x]!=null) {
-                    Rectangle rectangle = new Rectangle(33,33,Color.BLACK);
-                    playfield[posY+y][posX+x] = rectangle;
-                    figArray[y][x]=null;
-                }
-            }
-        }
-        figArray=randomizedFig();
         posY = 0;
         posX = 3;
-        for (int y = 0; y < figArray.length; y++) {
-            for (int x = 0; x < figArray[y].length; x++) {
-                if(figArray[y][x]!=null) {
-                    placeNode(posY+y,posX+x);
+
+        figur = randomizedFig();
+
+
+        for (int y = 0; y < figur.length; y++) {
+            for (int x = 0; x < figur[y].length; x++) {
+                if(figur[y][x]!=' ') {
+                    field[posY+y][posX+x] = figur[y][x];
                 }
             }
         }
     }
 
-    public Rectangle[][] randomizedFig(){
-        int[][] _figur;
-        Rectangle[][]rectFigur = new Rectangle[4][3];
+    public char[][] randomizedFig(){
+        char[][] _figur;
         int zufall=(int)Math.round(Math.random()*7);
 
         switch (zufall){
@@ -390,60 +406,7 @@ public class Controller implements Initializable {
                 break;
         }
 
-        for (int y = 0; y < _figur.length; y++) {
-            for (int x = 0; x < _figur[0].length; x++) {
-                if (_figur[y][x] == 1) {
-                    rectFigur[y][x] = new Rectangle(33, 33, Color.BLACK);
-                }
-            }
-        }
-        return rectFigur;
-    }
-
-    protected boolean rotate(int _y,int _x){
-        boolean collision = false;
-
-        int newY = posY+_y;
-        int newX = posX+_x;
-
-        // Fallunterscheidung nach den 4 erlaubten Winkel-Werten
-        // für die Berechnung der gedrehten Koordinaten zeileSpielfigur und spalteSpielfigur
-        switch (rotation) {
-            case 0: // abc
-                // def -> Original-Array, keine Veränderung
-                // Zeilen und Spalten unverändert übernehmen
-                newY = _y;
-                newX = _x;
-                break;
-
-            case 90: 	// da
-                // eb
-                // fc
-                // Zeilen der Spielfigur invertieren
-                // Zeilen und Spalten vertauschen
-                newY = _x;
-                newX = 3 - _y;
-                break;
-
-            case 180: 	// fed
-                // cba
-                // Zeilen und Spalten der Spielfigur invertieren
-                newY = 3 - _y;
-                newX = 2 - _x;
-                break;
-
-            case 270: 	// cf
-                // be
-                // ad
-                // Spalten der Spielfigur invertieren
-                // Zeilen und Spalten vertauschen
-                newX = _y;
-                newY = 2 - _x;
-                break;
-        }
-
-        return checkForCollision(newY+_y,newX+_x);
-
+        return _figur;
     }
 
 }
